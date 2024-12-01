d2c3feb430f64176623165fb5bd2a3ae08ed6984
Tiago Ferreira <thdejesusferreira@gmail.comgit config --listgit config --listgit config --list git config --list--list>
2024-12-01 13:58:15 -0300

    Criação da classe model

---
+++ asserts/js/pokemon-model.js
@@ -0,0 +1,17 @@
+class Pokemon{
+
+    number;
+    name;
+    type;
+    types=[];
+    img;
+
+    species;
+    height;
+    weight;
+    abilities=[];
+    gender=[];
+    eggGroups;
+    eggCycle;
+    libra;
+}
